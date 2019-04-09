2bb4fadd9415139eedff43445c129da78a1a24ad
petronics <peti.theobald@gmail.com>
2019-04-09 19:53:42 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit  {
+
+  rosesCount: number;
+  rcForm: FormGroup;
+
+  ngOnInit(): void {
+    this.rcForm = new FormGroup({
+      rosescount: new FormControl(null)
+    });
+  }
 }
